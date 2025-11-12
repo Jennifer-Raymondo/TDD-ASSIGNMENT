2c25ac9dfbccc6995b01ce9e5911fb9b711c868c
Jennifer-Raymondo <jenniferraymondo295@gmail.com>
2025-11-12 15:08:42 +0300

    made changes

--- borrow-book.test.js
+++ borrow-book.test.js
@@ -2,17 +2,17 @@
 const borrow_book = require('./borrow_book');
 
 test('borrows a book that exists', () => {
-    const books = ["Harry Potter", "The Alchemist", "Dune"];
+    const books = ["Percy Jackson", "The Alchemist", "Dune"];
     const msg = borrow_book("Dune", books);
     expect(msg).toBe("You have borrowed 'Dune'.");
-    expect(books).toEqual(["Harry Potter", "The Alchemist"]);
+    expect(books).toEqual(["Percy Jackson", "The Alchemist"]);
 });
 
 test('tries to borrow a book that is not available', () => {
-    const books = ["Harry Potter", "The Alchemist"];
+    const books = ["Percy Jackson", "The Alchemist"];
     const msg = borrow_book("The Hobbit", books);
     expect(msg).toBe("Sorry, 'The Hobbit' is not available.");
-    expect(books).toEqual(["Harry Potter", "The Alchemist"]);
+    expect(books).toEqual(["Percy Jackson", "The Alchemist"]);
 });
 
 test('borrowing from an empty list', () => {
